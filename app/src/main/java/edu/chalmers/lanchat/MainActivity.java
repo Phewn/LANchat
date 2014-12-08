3bdb9809eef1a3fc264ad2833b106cbfa416d1ae
Phewn <olivercarlsson89@gmail.com>
2014-12-08 17:57:03 +0100

    Simplest GUI no functionality

--- app/src/main/java/edu/chalmers/lanchat/MainActivity.java
+++ app/src/main/java/edu/chalmers/lanchat/MainActivity.java
@@ -5,21 +5,35 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity {
 
     private EditText newShout;
-    private TextView textI;
-    private Button shoutButton;
+    private ListView listViewI;
+    private ArrayList<String> listItems = new ArrayList<String>();
+    private ArrayAdapter<String> adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        newShout = (EditText) findViewById(R.id.chatLine);
+        listViewI = (ListView) findViewById(R.id.listViewI);
+
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listItems);
+
+        listViewI.setAdapter(adapter);
 
     }
 
@@ -51,9 +65,21 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    public void updateChatbox() {
+        String txt = newShout.getText().toString();
+        if (txt != "") {
+            listItems.add(txt);
+            adapter.notifyDataSetChanged();
+        }
+        return;
+    }
+
     public void ButtonOnClick(View v) {
         switch (v.getId()) {
             case R.id.shoutButton:
+                updateChatbox();
+                newShout.getText().clear();
+                break;
         }
     }
 }
